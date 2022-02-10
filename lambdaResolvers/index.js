7e28b8c9d328f7cfc79d6638f29bf8bea3be78e5
Sac <itssach.in>
2022-02-10 13:35:17 +0530

    - Added bit of state management and improvements to batch write

--- lambdaResolvers/index.js
+++ lambdaResolvers/index.js
@@ -2,7 +2,9 @@ const AWS = require('aws-sdk')
 const s3 = new AWS.S3()
 const ddbClient = new AWS.DynamoDB.DocumentClient()
 const moment = require('moment')
+const internal = require('stream')
 const uuidByString = require('uuid-by-string')
+const _ = require('lodash')
 
 module.exports.queryS3 = async (event, context) => {
   console.log('Event received to lambda', event)
@@ -10,18 +12,21 @@ module.exports.queryS3 = async (event, context) => {
     Bucket: process.env.S3_BUCKET,
     MaxKeys: 2
   }
-  const startAfterKey = await getStartAfterKey()
+  const [startAfterKey, interval] = await Promise.all([getStartAfterKey(), getItervalToProcess()])
   if (startAfterKey) query.StartAfter = startAfterKey
   let hasNextPage = true
   let iteration = 1
   let response = {}
-  while (hasNextPage) {
+  while (hasNextPage && context.getRemainingTimeInMillis() > 60000) {
     response = await s3.listObjectsV2(query).promise()
     console.log('Response ' + iteration++, response)
     if (response?.Contents?.length) {
       hasNextPage = response.IsTruncated
       query.StartAfter = response.Contents[response.Contents.length - 1].Key
-      await addS3ContentsToDDb(response.Contents)
+      await addS3ContentsToDDb(response.Contents.filter(content => {
+        const epochTimestamp = +new Date(content.LastModified)
+        return (epochTimestamp > interval.startTime && epochTimestamp < internal.endTime)
+      }))
     } else {
       hasNextPage = false
     }
@@ -42,29 +47,46 @@ module.exports.queryS3 = async (event, context) => {
 
 const addS3ContentsToDDb = async (items, tableName = 'ProcessedFilesTable') => {
   items = items.map(item => { return { id: uuidByString(item.Key), s3Key: item.Key, timestamp: +new Date(item.LastModified) } })
-  const payload = {
-    RequestItems: {
-      [tableName]: []
-    }
-  }
-  items.forEach(item => {
-    payload.RequestItems[tableName].push({
-      PutRequest: {
-        Item: item
+  const chunks = _.chunks(items, 20)
+  for (const chunk of chunks) {
+    const payload = {
+      RequestItems: {
+        [tableName]: []
       }
+    }
+    chunk.forEach(item => {
+      payload.RequestItems[tableName].push({
+        PutRequest: {
+          Item: item
+        }
+      })
     })
-  })
-  console.log('Payload for ddb', JSON.stringify(payload))
-  const response = await ddbClient.batchWrite(payload).promise()
-  console.log('ResponseFromDDb Write', response)
-  return response
+    // console.log('Payload for ddb', JSON.stringify(payload))
+    const response = await ddbClient.batchWrite(payload).promise()
+    console.log('ResponseFromDDb Write', response)
+  }
+  return true
 }
 
 const getStartAfterKey = async () => {
+  const stateRecord = await getStateItem('1')
+  return stateRecord?.s3Key || null
+}
+
+const getItervalToProcess = async () => {
+  const { startTime, endTime } = process.env
+  if (startTime && endTime) {
+    return { startTime, endTime }
+  } else {
+    const stateRecord = await getStateItem('2')
+    return stateRecord
+  }
+}
+
+function getStateItem (id) {
   const query = {
-    Key: '1',
+    Key: id,
     TableName: 'StateTable'
   }
-  const stateRecord = await ddbClient.getItem(query).promise()
-  return stateRecord?.s3Key || null
+  return ddbClient.getItem(query).promise()
 }
