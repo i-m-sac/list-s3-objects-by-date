e21b082229a2305dee005ab02cfa0689ed0ea7a9
Sac <itssach.in>
2022-02-10 07:31:35 +0530

    - Updating permissions for lambda functions and adding more developmental logs

--- lambdaResolvers/index.js
+++ lambdaResolvers/index.js
@@ -7,7 +7,18 @@ module.exports.queryS3 = async (event, context) => {
     Bucket: process.env.S3_BUCKET,
     MaxKeys: 2
   }
-  const response = await s3.listObjectsV2(query).promise()
-  console.log('Response from S3 Listing', response)
-  return response
+  console.log('Query for S3', query)
+  const response1 = await s3.listObjectsV2(query).promise().catch(err => {
+    console.log('Err while querying S3', { err, query })
+    throw err
+  })
+  console.log('Response from S3 Listing', JSON.stringify(response1))
+  query.ContinuationToken = response1.NextContinuationToken
+  const response2 = await s3.listObjectsV2(query).promise()
+  console.log('Response 2 ', JSON.stringify(response2))
+  delete query.ContinuationToken
+  query.StartAfter = response1.Contents[1].Key
+  const response3 = await s3.listObjectsV2(query).promise()
+  console.log('REsponse 3', JSON.stringify({ response3 }))
+  return response3
 }
